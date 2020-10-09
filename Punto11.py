7510fc8c1712e5e768ae870fcb69d8e51dcf0934
Julian Gallo <julianlondono@outlook.com>
2020-10-09 00:20:17 -0500

    ejercicio finalziado con los 12 puntos

--- Punto11.py
+++ Punto11.py
@@ -5,3 +5,15 @@ bachiller, en caso de no tenerlo, se puede acceder si hemos superado una prueba 
 acceso.
 """
 
+diploma = input("Confirme si o no en caso de que tenga un grado de bachiller: ")
+
+if diploma.lower() == "si":
+    print("Como tiene titulo bachiller Ud puede acceder a un ciclo formativo de grado superior")
+elif diploma.lower() == "no":
+    prueba = input("Confirme si ha hecho la prueba de acceso: ")
+    if prueba.lower() == "si":
+        print("Como no tiene grado bachiller pero si hizo la prueba de acceso Ud puede acceder a un ciclo formativo de grado superior")
+    else:
+        print("Como no tiene grado bachiller ni hizo la prueba de acceso Ud NO puede acceder a un ciclo formativo de grado superior")
+else:
+    print("Poner una respuesta valida")
---
+++ Punto12.py
@@ -0,0 +1,9 @@
+"""
+12) Desarrollar un algoritmo que nos calcule el cuadrado de los 9 primeros números
+naturales (recuerda la estructura desde-hasta)
+"""
+
+for i in range(9):
+    contador = i +1
+    cuadrado = contador**2
+    print("El cuadrado de " + str(contador) + " es: "+ str(cuadrado))
